cc6d3a9dc5a7a2214df0efb26132d879b27bf3a3
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-12-29 23:05:20 +0330

    ✨ task creation and editting view added

--- medadart/tasks/views.py
+++ medadart/tasks/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.generics import CreateAPIView, UpdateAPIView
+from rest_framework.permissions import IsAuthenticated
+from .serializers import TaskSerializer
+from .models import Task
 
-# Create your views here.
+
+class NewTask(CreateAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = TaskSerializer
+    queryset = Task.objects.all()
+
+class ChangeProgression(UpdateAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = TaskSerializer
+
+    def get_queryset(self):
+        return Task.objects.filter(creator=self.request.user)
