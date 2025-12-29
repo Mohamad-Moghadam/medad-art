572fc3431657e7cc5a99dd717ad0ae49005576c0
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-12-29 23:22:33 +0330

    ✨ jwt added

--- medadart/tasks/urls.py
+++ medadart/tasks/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from .views import NewTask, ChangeProgression
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
+    path("token/", TokenObtainPairView.as_view()),
+    path("refresh/", TokenRefreshView.as_view()),
     path("create/", NewTask.as_view()),
     path("update/", ChangeProgression.as_view()),
 ]
