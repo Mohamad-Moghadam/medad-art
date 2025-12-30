8029dcb2d3ae80ac4941b898b9c21fcf8129aeac
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-12-31 00:04:44 +0330

    ✨ urls added

--- medadart/tasks/urls.py
+++ medadart/tasks/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import NewTask, ChangeProgression, DeleteTask
+from .views import NewTask, ChangeProgression, DeleteTask, ListTasks
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
@@ -7,5 +7,6 @@ urlpatterns = [
     path("refresh/", TokenRefreshView.as_view()),
     path("create/", NewTask.as_view()),
     path("update/<int:pk>/", ChangeProgression.as_view()),
-    path("delete/<int:pk>/", DeleteTask.as_view())
+    path("delete/<int:pk>/", DeleteTask.as_view()),
+    path("list/", ListTasks.as_view()),
 ]
