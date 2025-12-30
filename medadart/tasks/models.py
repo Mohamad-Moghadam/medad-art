eff503ac062e9b26eb2339ffc880f7b4102fa6b2
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-12-30 13:23:53 +0330

    🐛 null blank true for progress

--- medadart/tasks/models.py
+++ medadart/tasks/models.py
@@ -10,7 +10,7 @@ class Task(models.Model):
     STATUS_CHOICES = [(TODO, "To Do"), (IN_PROGRESS, "In Progress"), (COMPLETED, "Completed")]
     title = models.CharField(max_length=50)
     description = models.TextField(blank=True, null=True)
-    progress = models.PositiveSmallIntegerField(default=0)
+    progress = models.PositiveSmallIntegerField(default=0, null=True, blank=True)
     status = models.CharField(max_length=20, choices=STATUS_CHOICES ,default=TODO)
 
     creator = models.ForeignKey(
